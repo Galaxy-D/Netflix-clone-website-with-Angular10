5c74f18817598274d972e18c76bf8bd92323bb5e
BlackOut <benlama.amine@gmail.com>
2020-11-02 11:59:59 +0100

    api key was added to environment file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  api_key : 'c0a9046ba0a0b67ec7e619fd8d56cc28'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  api_key : 'c0a9046ba0a0b67ec7e619fd8d56cc28'
 };
 
 /*
